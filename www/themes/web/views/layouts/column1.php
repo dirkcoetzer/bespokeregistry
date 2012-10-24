b530d6a97486a83dfec38f8582d920ac6c55ef0c
Johann Visagie <jdvisagie@gmail.com>
2012-10-24 14:48:51 +0200

    Deleted WWW directory

--- www/themes/web/views/layouts/column1.php
+++
@@ -1,9 +0,0 @@
-<?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
-	<div id="content">
-		<?php echo $content; ?>
-		<div class="clear"></div>
-	</div><!-- content -->
-	<div class="clear"></div>
-</div>
-<?php $this->endContent(); ?>
